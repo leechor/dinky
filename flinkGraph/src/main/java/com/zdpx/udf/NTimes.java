3452f8304a1075472e001425614b2abfd9e8c021
Licho <chinabhsun@gmail.com>
2022-11-21 11:38:18 +0800

    refactor: efact

--- flinkGraph/src/main/java/com/zdpx/udf/NTimes.java
+++ flinkGraph/src/main/java/com/zdpx/udf/NTimes.java
@@ -2,23 +2,25 @@ package com.zdpx.udf;
 
 import org.apache.flink.table.functions.ScalarFunction;
 
-/** */
+/**
+ *
+ */
 public class NTimes extends ScalarFunction implements IUdfDefine {
 
-  public long eval(Long input, int n) {
-    return input * n;
-  }
+    public long eval(Long input, int n) {
+        return input * n;
+    }
 
-  public double eval(Double input, int n) {
-    return input * n;
-  }
+    public double eval(Double input, int n) {
+        return input * n;
+    }
 
-  public String eval(String input, int n) {
-    return String.valueOf(Double.parseDouble(input) * n);
-  }
+    public String eval(String input, int n) {
+        return String.valueOf(Double.parseDouble(input) * n);
+    }
 
-  @Override
-  public String getUdfName() {
-    return "ntimes";
-  }
+    @Override
+    public String getUdfName() {
+        return "ntimes";
+    }
 }
